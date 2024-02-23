7f3d73f92a429b327a9361433ce174a7ff567c0e
Savy92 Saddall <savy92@hotmail.co.uk>
2024-02-23 17:59:20 +0000

    add form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://drf-api-savy-4b5b4afdeea3.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
